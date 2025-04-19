0901bdb2c57bf50bac75021a948a1348394ec337
Vishnu <vishnuvardhanganji@gmail.com>
2025-04-19 22:40:32 +0530

    auth and storage

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -12,6 +12,9 @@ import Link from "next/link";
 import { toast } from "sonner";
 import FormField from "./FormField";
 import { useRouter } from "next/navigation";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "@/firebase/client";
+import { signIn, signUp } from "@/lib/actions/auth.actions";
 
 const authFormSchema = (type: FormType) => {
   return z.object({
@@ -32,14 +35,37 @@ const AuthForm = ({ type }: { type: FormType }) => {
       password: "",
     },
   })
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  async function onSubmit(values: z.infer<typeof formSchema>) {
     try {
       if (type === "sign-up") {
         console.log('SIGN UP: ', values)
+        const { name, email, password } = values
+        const userCredentials = await createUserWithEmailAndPassword(auth, email, password)
+        const result = await signUp({
+          uid: userCredentials.user.uid,
+          name: name!,
+          email,
+          password
+        })
+        if (!result?.success) {
+          toast.error(result.message)
+          return
+        }
         toast.success('Sign up successful')
         router.push('/sign-in')
       } else {
         console.log('SIGN IN: ', values)
+        const { email, password } = values
+        const userCredentials = await signInWithEmailAndPassword(auth, email, password)
+        const idToken = await userCredentials.user.getIdToken()
+        if (!idToken) {
+          toast.error('Failed to sign in')
+          return
+        }
+        await signIn({
+          email,
+          idToken
+        })
         toast.success('Sign in successful')
         router.push('/')
       }
